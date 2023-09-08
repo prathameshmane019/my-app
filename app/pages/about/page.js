d62d6f250fa5ed74c7a250e05203e77fd47b19f7
prathameshmane019 <maneprathamesh019@gmail.com>
2023-09-09 00:08:40 +0530

    Fourth Structure

--- app/pages/about/page.js
+++ app/pages/about/page.js
@@ -1,6 +1,9 @@
-export default function Home() {
+import About from "./about";
+
+export default function AboutPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
+    <About />
     </main>
-  )
+  );
 }
