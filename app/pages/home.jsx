ccbec5a23bfa48c77e27bb2380fcb5a106d2a094
prathameshmane019 <maneprathamesh019@gmail.com>
2023-09-08 20:23:19 +0530

    First Structure

---
+++ app/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Carousel from '../components/carousel'
+
+const HomePage = () => {
+  return (
+    <div>
+      {/* <Carousel/> */}
+    </div>
+  )
+}
+
+export default HomePage
