3f7524141df105bed8f522729622c6c2856804bf
prathameshmane019 <maneprathamesh019@gmail.com>
2023-09-09 21:34:01 +0530

    changes maid

---
+++ app/Main/ministry/ministry-home/page.js
@@ -0,0 +1,8 @@
+
+export default function DashboardPage() {
+
+  return(
+    <div>This is ministry home page</div>
+  );
+
+}
--- app/Main/ministry/page.js
+++ app/Main/ministry/page.js
@@ -1,9 +1,11 @@
 import MLogin from "./m-login";
+import Projects from "./projects";
 
 export default function MinistryPage() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between ">
       <MLogin />
+      <Projects/>
       </main>
     )
   }
