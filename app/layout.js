2dd5f0186c93d7e4ce3fe01884ddb5fcf032c29b
prathameshmane019 <maneprathamesh019@gmail.com>
2023-09-10 11:05:27 +0530

    sample update 1.2

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       {" "}
       <body className={inter.className}>
-      <Header />
+      <Header/>
         {children}
       <Footer />
       </body>
