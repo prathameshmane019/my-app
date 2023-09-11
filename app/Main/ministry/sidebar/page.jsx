f39cdf9c390440b8b3e4289675165690f77ff603
Madhav Kamble <madhavukamble@gmail.com>
2023-09-11 19:50:38 +0530

    updated the menus in sidebar of ministry (refined)

--- app/Main/ministry/sidebar/page.jsx
+++ app/Main/ministry/sidebar/page.jsx
@@ -52,7 +52,7 @@ export default function MinistrySideBar() {
             d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
           />
         </svg>
-        Add Projects
+        Add University
       </div>
     </div>
   );
