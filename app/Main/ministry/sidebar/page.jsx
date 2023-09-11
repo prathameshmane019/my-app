1a19803e1b842212c2327aca1e51e987bc6ba5ae
Madhav Kamble <madhavukamble@gmail.com>
2023-09-11 16:36:01 +0530

    updated the menus in sidebar of ministry

--- app/Main/ministry/sidebar/page.jsx
+++ app/Main/ministry/sidebar/page.jsx
@@ -1,8 +1,6 @@
 export default function MinistrySideBar() {
   return (
     <div className="h-full w-64 border-r pt-10 px-5">
-      <p className="text-xs font-medium text-gray-400">MAIN</p>
-
       <div className="mt-4 py-1.5 text-sm font-medium text-slate-500 hover:text-blue-500 group cursor-pointer flex items-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -19,79 +17,6 @@ export default function MinistrySideBar() {
           />
         </svg>
         Dashboard
-      </div>
-
-      <p className="text-xs font-medium text-gray-400 mt-8">APPLICATIONS</p>
-      <div className="mt-4 py-1.5 text-sm font-medium text-slate-500 hover:text-blue-500 group cursor-pointer flex items-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 stroke-slate-400 mr-4 group-hover:stroke-blue-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-          />
-        </svg>
-        Calender
-      </div>
-
-      <div className="mt-4 py-1.5 text-sm font-medium  text-blue-500 group cursor-pointer flex items-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5   mr-4  stroke-blue-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
-          />
-        </svg>
-        Messages
-      </div>
-
-      <div className="mt-4 py-1.5 text-sm font-medium text-slate-500 hover:text-blue-500 group cursor-pointer flex items-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 stroke-slate-400 mr-4 group-hover:stroke-blue-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-          />
-        </svg>
-        Contacts
-      </div>
-
-      <div className="mt-4 py-1.5 text-sm font-medium text-slate-500 hover:text-blue-500 group cursor-pointer flex items-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 stroke-slate-400 mr-4 group-hover:stroke-blue-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
-          />
-        </svg>
-        Team Members
       </div>
 
       <div className="mt-4 py-1.5 text-sm font-medium text-slate-500 hover:text-blue-500 group cursor-pointer flex items-center">
