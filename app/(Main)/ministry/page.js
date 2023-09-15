65a952d876fbca435b01e06bc9d0e0423a5162c5
Madhav Kamble <madhavukamble@gmail.com>
2023-09-15 14:47:57 +0530

    feature added :login pages structure

--- app/(Main)/ministry/page.js
+++ app/(Main)/ministry/page.js
@@ -1,8 +1,24 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
-import DashboardPage from "./dashboard/page";
 
 const Ministry = () => {
-  return <DashboardPage />;
+  const router = useRouter();
+
+  const navigate = (name) => {
+    router.push("/ministry/features" + name);
+  };
+  return (
+    <>
+      <h1>This is minifddfstry login page</h1>
+      <button
+        className="btn btn-primary"
+        onClick={() => navigate("/dashboard")}
+      >
+        Primary
+      </button>
+    </>
+  );
 };
 
 export default Ministry;
