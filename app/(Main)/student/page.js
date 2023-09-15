bdd3277db92eb65e6761718f72793d5bc0ee919a
kalyanilanadge05 <kalyanilandage2003@gmail.com>
2023-09-15 18:34:16 +0530

    base structure modified

--- app/(Main)/student/page.js
+++ app/(Main)/student/page.js
@@ -71,8 +71,8 @@ const Student = () => {
                 Login
               </button>
               <p className="mt-10 text-center text-sm text-gray-500">
-                You have Register? {' '}
-                <Link href="/register" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                Don't have a account? {' '}
+                <Link href="/student/register" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
                   Register Now
                 </Link>
               </p>
--- app/(Main)/student/register/page.js
+++ app/(Main)/student/register/page.js
@@ -11,12 +11,7 @@ const Student = () => {
   return (
     <>
       <h1>This is student registration page</h1>
-      <button
-        className="btn btn-primary"
-        onClick={() => navigate("features/dashboard")}
-      >
-        Login
-      </button>
+      
       <button className="btn btn-primary" onClick={() => navigate("/")}>
         Register
       </button>
